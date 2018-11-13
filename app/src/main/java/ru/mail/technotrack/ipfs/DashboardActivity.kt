b2cebbb5c585dbaeca82be8647de70aae121b271
Rustam Sitdikov <sitdikov.rs@yandex.ru>
2018-11-13 04:49:30 +0300

    added new dashboard activity

--- app/src/main/java/ru/mail/technotrack/ipfs/DashboardActivity.kt
+++ app/src/main/java/ru/mail/technotrack/ipfs/DashboardActivity.kt
@@ -1,37 +1,104 @@
 package ru.mail.technotrack.ipfs
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayout
 
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardActivity : AppCompatActivity() {
 
-    companion object {
-        fun start(context: Context) {
-            context.startActivity(Intent(context, DashboardActivity::class.java))
-        }
-    }
-
-    lateinit var viewPager: ViewPager
-    lateinit var tabLayout: TabLayout
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        viewPager = findViewById(R.id.viewpager_dashboard)
-        tabLayout = findViewById(R.id.tablayout_dashboard)
+        setSupportActionBar(toolbar_dashboard)
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
-        val adapter = DashboardViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(DashboardTabFragment(), "Tab 1")
-        adapter.addFragment(DashboardTabFragment(), "Tab 2")
-        adapter.addFragment(DashboardTabFragment(), "Tab 3")
+        // Set up the ViewPager with the sections adapter.
+        container.adapter = mSectionsPagerAdapter
 
-        viewPager.adapter = adapter
-        tabLayout.setupWithViewPager(viewPager)
+        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs_dashboard))
+        tabs_dashboard.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_dashboard, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+    
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a DashboardTabFragment (defined as a static inner class below).
+            return DashboardTabFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 3
+        }
+    }
+
+    class DashboardTabFragment : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val rootView = inflater.inflate(R.layout.fragment_dashboard, container, false)
+            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
+            return rootView
+        }
+
+        companion object {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private val ARG_SECTION_NUMBER = "section_number"
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(sectionNumber: Int): DashboardTabFragment {
+                val fragment = DashboardTabFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
     }
 }
