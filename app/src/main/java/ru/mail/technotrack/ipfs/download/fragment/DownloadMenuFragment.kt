a6b4ddf662d90d4ccfac5c162b28218e96970b8c
Arkadii Kudashov <ark100295@yandex.ru>
2018-12-22 03:26:21 +0300

    add choosing pictures from gallery and files from storage (need to fix)

--- app/src/main/java/ru/mail/technotrack/ipfs/download/fragment/DownloadMenuFragment.kt
+++ app/src/main/java/ru/mail/technotrack/ipfs/download/fragment/DownloadMenuFragment.kt
@@ -1,6 +1,7 @@
 package ru.mail.technotrack.ipfs.download.fragment
 
 import android.Manifest
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -15,10 +16,18 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_download_menu.*
 
 import ru.mail.technotrack.ipfs.R
+import android.graphics.Bitmap
+import android.widget.ImageView
+
 
 class DownloadMenuFragment : BottomSheetDialogFragment() {
 
     private val REQUEST_CAMERA = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
+    private val REQUEST_READ_EXTERNAL_FOR_GALLERY = 3
+    private val REQUEST_PICK_IMAGE = 4
+    private val REQUEST_READ_EXTERNAL_FOR_STORAGE = 5
+    private val REQUEST_PICK_FILE = 6
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_download_menu, container, false)
@@ -49,6 +58,42 @@ class DownloadMenuFragment : BottomSheetDialogFragment() {
                         true
                     }
                 }
+                menuItem.itemId == R.id.item_gallery -> {
+                    if (ContextCompat.checkSelfPermission(
+                            context!!,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            activity!!,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_FOR_GALLERY)
+                        dismiss()
+                        true
+                    } else {
+                        dispatchSelectPictureFromGalleryIntent()
+                        dismiss()
+                        true
+                    }
+                }
+                menuItem.itemId == R.id.item_storage -> {
+                    if (ContextCompat.checkSelfPermission(
+                            context!!,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            activity!!,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_FOR_STORAGE)
+                        dismiss()
+                        true
+                    } else {
+                        dispatchSelectFileFromStorageIntent()
+                        dismiss()
+                        true
+                    }
+                }
                 else -> {
                     dismiss()
                     false
@@ -56,8 +101,6 @@ class DownloadMenuFragment : BottomSheetDialogFragment() {
             }
         }
     }
-
-    val REQUEST_IMAGE_CAPTURE = 1
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -67,11 +110,65 @@ class DownloadMenuFragment : BottomSheetDialogFragment() {
         }
     }
 
+    private fun dispatchSelectPictureFromGalleryIntent() {
+        Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            .also {galleryIntent ->
+                galleryIntent.resolveActivity(context!!.packageManager)?.also {
+                    startActivityForResult(galleryIntent, REQUEST_PICK_IMAGE)
+                }
+            }
+    }
+
+    private fun dispatchSelectFileFromStorageIntent() {
+//        val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+//        getIntent.type = "image/*"
+//
+//        val pickIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        pickIntent.type = "image/*"
+//
+//        val chooserIntent = Intent.createChooser(getIntent, "Select Image")
+//        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+//
+//        startActivityForResult(chooserIntent, REQUEST_PICK_IMAGE)
+
+//        Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        Intent(Intent.ACTION_GET_CONTENT)
+            .also {storageIntent ->
+                storageIntent.resolveActivity(context!!.packageManager)?.also {
+                    startActivityForResult(storageIntent, REQUEST_PICK_FILE)
+                }
+            }
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CAMERA && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
             dispatchTakePictureIntent()
         }
+
+        if (requestCode == REQUEST_READ_EXTERNAL_FOR_GALLERY && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+            dispatchSelectPictureFromGalleryIntent()
+        }
+
+        if (requestCode == REQUEST_READ_EXTERNAL_FOR_STORAGE && grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
+            dispatchSelectFileFromStorageIntent()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val photo = data!!.extras.get("data") as Bitmap
+            val imageView = this.activity!!.findViewById<ImageView>(R.id.imageView)
+            imageView.setImageBitmap(photo)
+        }
+
+        if (requestCode == REQUEST_PICK_IMAGE && resultCode == RESULT_OK) {
+
+        }
+
     }
 
 }
